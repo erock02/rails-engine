c918267740d2a65b7bddb61ff5c2c7eed3f5d997
Eric Espindola <eric_e_2012@hotmail.com>
2022-07-14 03:19:18 -0700

    create tests and update route/controller for item#destroy

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Name.name }
     description { Faker::Lorem.paragraph }
-    unit_price { Faker::Number.within(range: 1..10) }
+    unit_price { Faker::Number.within(range: 1.0..10.0) }
 
     association :merchant, factory: :merchant
   end
